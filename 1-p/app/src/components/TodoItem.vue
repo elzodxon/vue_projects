ad5cc76e65e69e3cac313ab68727c5d818a9256b
elzodxon <sharofaddinov01@mail.ru>
2020-12-01 13:23:40 +0500

    Finished Todo app

---
+++ 1-p/app/src/components/TodoItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <div class="todo-first" style="display: flex; align-items: center;">
+      <input type="checkbox" v-on:change="markComplete">
+      <p>{{todo.title}}</p>
+    </div>
+    <button @click="$emit('del-todo', todo.id)" class="del">X</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ['todo'],
+  methods: {
+    markComplete(){
+      this.todo.completed = !this.todo.completed
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .todo-item{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+
+  .is-complete{
+    text-decoration: line-through;
+  }
+  .del{
+    background: #ff0000;
+    color: white;
+    border-radius: 50%;
+    border: none;
+    height: 30px;
+    width: 30px;
+  }
+
+  input[type='checkbox']{
+    margin-right: 20px;
+  }
+</style>
---
+++ 1-p/app/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <div class="container">
+      <div class="wrapper">
+        <h1>To do list has been created by Elzodxon</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .container{
+    margin: 0 auto;
+    padding: 20px;
+  }
+</style>
